421cf196b256f8b14ef538781425ce157fe93148
Thomas Tibbetts <tomtibbetts@gmail.com>
2021-02-06 16:37:06 -0600

    Update ATX-Watchdog_startupsetup.sh

--- scripts/ATX-Watchdog_startupsetup.sh
+++ scripts/ATX-Watchdog_startupsetup.sh
@@ -8,9 +8,9 @@ GPIO.setmode(GPIO.BCM)
 
 pulseStart = 0.0
 SHUTDOWN = 24               #pin 18
-ATX_WATCHDOG_ADDRESS = 0x5A	#I2C address
-BOOT_OK_COMMAND = 0x83		#Set Boot Ok process
-BOOT_OK = 0x01				#Signal that we booted up okay
+ATX_WATCHDOG_ADDRESS = 0x5A #I2C address
+BOOT_OK_COMMAND = 0x83      #Set Boot Ok process
+BOOT_OK = 0x01              #Signal that we booted up okay
 REBOOTPULSEMINIMUM = 0.2    #reboot pulse signal should be at least this long
 REBOOTPULSEMAXIMUM = 0.6    #reboot pulse signal should be at most this long
 
@@ -20,9 +20,9 @@ GPIO.setup(SHUTDOWN, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
 try:
     print ("\n== Signalling Boot Ok\n")
-	bus = smbus.SMBus(1)
-	bus.write_byte_data(ATX_WATCHDOG_ADDRESS, BOOT_OK_COMMAND, BOOT_OK)
-	bus.close()
+    bus = smbus.SMBus(1)
+    bus.write_byte_data(ATX_WATCHDOG_ADDRESS, BOOT_OK_COMMAND, BOOT_OK)
+    bus.close()
 	
     while True:
         print ("\n== Waiting for shutdown pulse\n")
@@ -55,9 +55,9 @@ echo '#!/usr/bin/python
 
 import smbus
 
-ATX_WATCHDOG_ADDRESS = 0x5A	#I2C address
-BOOT_OK_COMMAND = 0x83		#Set Boot Ok process
-BOOT_NOT_OK = 0x00			#Signal that we are shutting down
+ATX_WATCHDOG_ADDRESS = 0x5A #I2C address
+BOOT_OK_COMMAND = 0x83      #Set Boot Ok process
+BOOT_NOT_OK = 0x00          #Signal that we are shutting down
 
 bus = smbus.SMBus(1)
 bus.write_byte_data(ATX_WATCHDOG_ADDRESS, BOOT_OK_COMMAND, BOOT_NOT_OK)
