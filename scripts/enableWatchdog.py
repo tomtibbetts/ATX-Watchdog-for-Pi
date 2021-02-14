df2bfad90718289d3bc11712da291dccd8174698
Thomas Tibbetts <tomtibbetts@gmail.com>
2021-02-14 08:57:12 -0600

    Updated Files

--- scripts/enableWatchdog.py
+++ scripts/enableWatchdog.py
@@ -5,11 +5,11 @@ import time
 
 ATX_WATCHDOG_ADDRESS = 0x5A        #I2C address
 WATCHDOG_ENABLE_COMMAND = 0x35     #Access the watchdog Register
-WATCHDOG_DISABLE = 0x00            #Disable the watchdog, reset the reboot counter
+WATCHDOG_ENABLE = 0x82             #Enable the watchdog, Reboot up to two times
 
 bus = smbus.SMBus(1)
 
-bus.write_byte_data(ATX_WATCHDOG_ADDRESS, WATCHDOG_ENABLE_COMMAND, WATCHDOG_DISABLE)
+bus.write_byte_data(ATX_WATCHDOG_ADDRESS, WATCHDOG_ENABLE_COMMAND, WATCHDOG_ENABLE)
 bus.close()
 
 
