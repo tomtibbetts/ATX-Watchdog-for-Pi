028b0007104a21ba53b756d4fe4bff86057f7cf1
Thomas Tibbetts <tomtibbetts@gmail.com>
2021-02-14 08:47:36 -0600

    Updated sendBootOk

--- scripts/sendBootOk.py
+++ scripts/sendBootOk.py
@@ -2,11 +2,13 @@
 
 import smbus
 
+ATX_WATCHDOG_ADDRESS = 0x5A #I2C address
+BOOT_OK_COMMAND = 0x83      #Set Boot Ok process
+BOOT_OK = 0x01              #Signal that we are shutting down
+
 
 bus = smbus.SMBus(1)
 
-
-DEVICE_ADDRESS = 0x5A
-bus.write_byte_data(DEVICE_ADDRESS, 0x83, 1)
+bus.write_byte_data(ATX_WATCHDOG_ADDRESS, BOOT_OK_COMMAND, BOOT_OK)
 bus.close()
 
