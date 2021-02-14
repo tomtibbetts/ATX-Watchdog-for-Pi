0e1b17da7f7688a1faa359fa5efa043c2f403b6f
Thomas Tibbetts <tomtibbetts@gmail.com>
2021-02-14 09:00:42 -0600

    updated script

--- scripts/resetWatchdogEverySecond.py
+++ scripts/resetWatchdogEverySecond.py
@@ -3,14 +3,13 @@
 import smbus
 import time
 
-
-
-DEVICE_ADDRESS = 0x5A
+ATX_WATCHDOG_ADDRESS = 0x5A #I2C address
+WATCHDOG_KEEP_ALIVE = 0x82  #Ping ATX Watchdog command
 
 while 1:
     print("Resetting Watchdog")
     bus = smbus.SMBus(1)
-    bus.write_byte_data(DEVICE_ADDRESS, 0x82, 0)
+    bus.write_byte_data(ATX_WATCHDOG_ADDRESS, WATCHDOG_KEEP_ALIVE, 0)
     bus.close()
     time.sleep(100)
     
